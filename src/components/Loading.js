ab54b556b7e18ec7a3ecb29c741259482c002304
Sean Barker <sean.p.barker86@gmail.com>
2021-11-29 21:27:24 -0600

    chipping away at search and preview

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
-/**
- * Create and export a component called Loading which uses this static HTML template:
- * 
- * <div id="loading">
- *   <h2 className="message">Searching...</h2>
- * </div>
- */
+// Create and export a component called Loading which uses this static HTML template:
+
+export default function Loading() {
+  return (
+    <div id="loading">
+      <h2 className="message">Searching...</h2>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 // These imports won't work until you fix ./components/index.js
-import {
-  Feature,
-  Loading,
-  Preview,
-  Search,
-  Title
-} from './components';
+import { Feature, Loading, Preview, Search, Title } from "./components";
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [featuredResult, setFeaturedResult] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+
   /**
    * We are at the App level component, which is top-most. Any state which needs to be shared between immediate children should
    * be made here, so create state pairs using useState() for:
-   * 
+   *
    * searchResults, setSearchResults (default should be this object:  {info: {}, records: []} )
    * featuredResult, setFeaturedResult (default should be null)
    * isLoading, setIsLoading (default should be false)
    */
 
-  return <div className="app">
-    {/* <Title /> is static, doesn't need any props */}
-    <Title />
-    {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview) */}
-    <Search />
-    {/* <Preview /> needs props for searchResults, setIsLoading and setSearchResults (clicking prev/next buttons), and setFeaturedResult (clicking a preview) */}
-    <Preview />
-    {/* <Feature /> needs props for featuredResult, as well as setIsLoading and setSearchResults (clicking on searchable properties) */}
-    <Feature />
-    {/* <Loading /> is static, but should only render when isLoading is true */}
-    <Loading /> {/* use a ternary and render null if isLoading is false */}
-  </div>
-}
+  return (
+    <div className="app">
+      {/* <Title /> is static, doesn't need any props */}
+      <Title />
+      {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview) */}
+      <Search setIsLoading={setIsLoading} setSearchResults={setSearchResults} />
+      {/* <Preview /> needs props for searchResults, setIsLoading and setSearchResults (clicking prev/next buttons), and setFeaturedResult (clicking a preview) */}
+      <Preview
+        searchResults={searchResults}
+        setFeaturedResult={setFeaturedResult}
+        setSearchResults={setSearchResults}
+        setIsLoading={setIsLoading}
+      />
+      {/* <Feature /> needs props for featuredResult, as well as setIsLoading and setSearchResults (clicking on searchable properties) */}
+      <Feature
+        featuredResult={featuredResult}
+        setIsLoading={setIsLoading}
+        setSearchResults={setSearchResults}
+      />
+      {/* <Loading /> is static, but should only render when isLoading is true */}
+      <Loading /> {/* use a ternary and render null if isLoading is false */}
+    </div>
+  );
+};
 
 /**
  * Boostrap the <App /> component into the '#app' element in the DOM,
  * using ReactDOM.render();
  */
+ReactDOM.render(<App />, document.getElementById("app"));
